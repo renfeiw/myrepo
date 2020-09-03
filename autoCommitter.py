53271b1c917cb0e95b2548a932f34d2f54f59b5e
renfeiw <renfeiw@ca.ibm.com>
2020-09-03 16:12:13 -0400

    test disable

---
+++ autoCommitter.py
@@ -0,0 +1,111 @@
+import git
+import re
+import os
+import argparse
+from pathlib import Path
+import shutil
+import random
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--file", required=True, help="regex file name to modify")
+    ap.add_argument("-s", "--find", required=True, help="regex string to search in file")
+    ap.add_argument("-r", "--replace", required=True, help="regex string to replace found")
+    ap.add_argument("-m", "--message", required=True, help="commit message")
+    ap.add_argument("-o", "--originURL", required=True, help="git origin repo")
+    run(vars(ap.parse_args()))
+
+
+def run(args):
+    print(f"Attempting to create auto PR against {args['originURL']}\n")
+    workingPath = os.path.join(os.getcwd(), "tempRepo")
+    removeDir(workingPath)
+    originBranch = "autoBranch" + str(random.randint(0,10000))
+    print("- getting material from remote")
+    repo = setupRepo(workingPath, args["originURL"], originBranch)
+    print(f"- searching file {args['file']} in working branch")
+    files = find_files(args["file"], workingPath)
+    print(f"- updating file(s)")
+    isUpdated = updateFiles(files, args["find"], args["replace"])
+    if isUpdated:
+        print(f"- commit and push change to remote")
+        gitCommitPush(repo, originBranch, args["message"])
+        print(f"- creating PR")
+        return True
+    else:
+        print("abort: nothing to update\n")
+        return False
+#    removeDir(workingPath)
+    
+    
+def removeDir(workingPath):
+    path = Path(workingPath)
+    if path.exists() and path.is_dir():
+        shutil.rmtree(path)
+
+
+def find_files(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            if re.match(name, filename):
+                result.append(os.path.join(root, filename))
+                print(".", end="")
+    print("\n")
+    return result
+
+
+def updateFiles(files, find, replace):
+    for file in files:
+        pattern = re.compile(find)
+        with open(file) as inFile:
+            input = inFile.read()
+            search = re.search(find, input)
+            if search is not None:
+                output = re.sub(find, replace, input)
+                temp = file + ".temp"
+                with open(temp, "w") as outFile:
+                    outFile.write(output)
+                    os.remove(file)
+                    os.rename(temp, file)
+                    print(f"updated {file}\n")
+                    return True
+    return False
+        
+
+def setupRepo(path, originURL, originBranch):
+    repo = git.Repo.clone_from(originURL, path)
+    print(f"cloned from origin {originURL}")
+
+    repo.git.branch(originBranch)
+    repo.git.checkout(originBranch)
+    print(f"created working branch {originBranch}")
+
+#    upstream = repo.create_remote("upstream", url=upstreamURL)
+#    upstream.fetch()
+#    repo.git.reset("--hard", f"upstream/{upstreamBranch}")
+#    print(f"reset based on upstream {upstreamURL}/{upstreamBranch}\n")
+    return repo
+
+
+def gitCommitPush(repo, originBranch, disableMsg):
+    changed = [ item.a_path for item in repo.index.diff(None) ] 
+    if changed:
+        repo.git.add(".")
+        print(repo.git.commit("-s", "-m", disableMsg))
+        origin = repo.remote(name="origin")
+        result = origin.push(refspec=f"{originBranch}:{originBranch}")
+        print(f"change pushed from {result[0].local_ref} to {result[0].remote_ref}\n")
+
+        
+"""
+curl \
+  -X POST \
+  -H "Authorization: token <git token>" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/llxia/openj9/pulls \
+  -d "{"title":"my test PR","head":"asm","base":"master"}"  
+"""
+ 
+if __name__ == "__main__":
+    main()
---
+++ disable.py
@@ -0,0 +1,58 @@
+import autoCommitter
+import argparse
+import re
+
+
+ORIGIN_OPENJ9 = "https://github.com/renfeiw/myrepo.git"
+
+
+"""
+ORIGIN_ADOPTOPENJDK = "https://github.com/renfeiw/openjdk-tests.git"
+UPSTREAM_ADOPTOPENJDK = "https://github.com/AdoptOpenJDK/openjdk-tests.git"
+UPSTREAM_ADOPTOPENJDK_BRANCH = "master"
+
+ORIGIN_OPENJ9 = "https://github.com/renfeiw/openj9.git"
+UPSTREAM_OPENJ9 = "https://github.com/eclipse/openj9.git"
+UPSTREAM_OPENJ9_BRANCH = "master"
+"""
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-m", "--mode", required=True, help="operation on test, supported mode: disable")
+    ap.add_argument("-t", "--test", required=True, help="test name")
+    ap.add_argument("-i", "--issue", required=True, help="issue URL")
+    ap.add_argument("-f", "--file", default="playlist.xml", help="regex file name to modify")
+    args = vars(ap.parse_args())
+    
+    newArgs = {
+        "file": args["file"]
+    }
+
+    if (args["mode"] == "disable"):
+        if (re.match(args["file"], r"playlist.xml")):
+            disableTestInPlaylist(args["test"], args["issue"], newArgs)
+
+
+def disableTestInPlaylist(test, issue, args):
+    print(f"Creating auto PR to disable {test} in {args['file']}\n")
+
+    args.update({
+        "find" : fr"(([ \t]*)<\s*testCaseName\s*>\s*{test}\s*<\s*/testCaseName\s*>)",
+        "replace": fr"\1\n\2<disabled>{issue}</disabled>",
+        "message": f"Auto: disable {test}\n- related issue: {issue}"})
+    
+    rt = False
+    args.update({
+        "originURL": ORIGIN_OPENJ9})
+    rt = autoCommitter.run(args)
+"""
+    args.update({
+        "originURL": ORIGIN_ADOPTOPENJDK,
+        "upstreamURL": UPSTREAM_ADOPTOPENJDK,
+        "upstreamBranch": UPSTREAM_ADOPTOPENJDK_BRANCH})
+    rt or autoCommitter.run(args)
+"""
+
+if __name__ == "__main__":
+    main()
+    
